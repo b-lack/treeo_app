adee121b083444554b70c85ebf4a7abd764d8b48
Gerrit Balindt <gerrit.balindt@b-lack.io>
2019-06-04 21:37:07 +0200

    add submodule

--- treeo_webview/src/data/config_live_example.js
+++
@@ -1,9 +0,0 @@
-export default
-{
-  'name': 'stage',
-  'base': 'https://live.example.com',
-  'mediumMedia': '/media/medium/',
-  'squareMedia': '/media/large_square/',
-  'forgotPasswordBase': 'https://live.example.com/password-reset',
-  'pinToEnter': '7823'
-}
